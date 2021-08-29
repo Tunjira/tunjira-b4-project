9ec6f33bdfe1fbf6bda7f89cccbd818c61751b65
Tunjira Hunhaboon <thanjira@tunjiras-mbp.myfiosgateway.com>
2021-08-29 10:01:21 -0400

    add new file

---
+++ src/com/company/GitPractice.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class GitPractice {
+    public static void main(String[] args) {
+        System.out.println("This is new file to be pushed") {
+
+        }
+    }
+}
